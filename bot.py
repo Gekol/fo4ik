3bedaf2790314f867ccb04f136be49c60a753fb5
Vlad Zinkovsky <terraemw@gmail.com>
2019-07-20 21:06:31 +0300

    add new button

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import telebot
 import cfg
-bot = telebot.TeleBot(cfg.TOKEN)
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Привет', 'Пока', 'Как дела', 'Скинь песню')
-chat_id = 'https://t.me/SvRT_group'
 
+bot = telebot.TeleBot(cfg.TOKEN)
+keyboard2 = telebot.types.ReplyKeyboardMarkup()
+keyboard2.row('Привет', 'Как дела', 'Скинь песню', 'Предыдущая страница')
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('Инструкция', 'О боте', 'Поддержать нас', 'Следующая страница')
 
 
 
@@ -15,17 +16,25 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой Создатель', reply_markup=keyboard1)
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, Создатель',reply_markup=keyboard1)
+        bot.send_message(message.chat.id, 'Привет, мой Создатель', reply_markup=keyboard2)
     elif message.text.lower()== 'как дела':
-        bot.send_message(message.chat.id, 'Отлично, в отличии от тебя нисшее создание',reply_markup=keyboard1)
+        bot.send_message(message.chat.id, 'Отлично, в отличии от тебя нисшее создание',reply_markup=keyboard2)
     elif message.text.lower() == 'скинь песню':
         voice =  open ('Music/POWERWOLF - Demons Are A Girls Best Friend (Cover by Radio Tapok  на русском).mp3', 'rb')
-        bot.send_audio(message.chat.id, voice, reply_markup=keyboard1)
+        bot.send_audio(message.chat.id, voice, reply_markup=keyboard2)
         file_id = 'pwrt'
         bot.send_document(message.chat.id, file_id)
 
+    elif message.text.lower() == 'инструкция':
+        bot.send_message(message.chat.id, 'Инструкция временно отсутствует', reply_markup=keyboard1)
+    elif message.text.lower() == 'о боте':
+        bot.send_message(message.chat.id, 'Это бот Вася', reply_markup=keyboard1)
+    elif message.text.lower()== 'поддержать нас':
+        bot.send_message(message.chat.id, 'Порекомендуйте меня друзьям. Это самая лучшая помощь', reply_markup=keyboard1)
+    elif message.text.lower() == 'следующая страница':
+        bot.send_message(message.chat.id, 'Выполняю',  reply_markup=keyboard2)
+    elif message.text.lower() == 'предыдущая страница':
+        bot.send_message(message.chat.id, 'Выполняю',  reply_markup=keyboard1)
 
 
 
